c4625e32d4afc579b33cc8a6794d8241d96dfb0e
LadeMor <sergpyatko@gmail.com>
2025-02-20 16:11:51 +0200

    Added products selector

--- models/productModel.js
+++ models/productModel.js
@@ -30,7 +30,31 @@ const Product = {
 
   getByOrderId: (orderId, limit, offset) =>
     db.any(
-      "SELECT * FROM products WHERE order_id = $1 LIMIT $2 OFFSET $3",
+      "SELECT " +
+      " p.id AS product_id, " +
+      " p.title, " +
+      " p.order_id, " +
+      " json_agg(" +
+      " DISTINCT jsonb_build_object(" +
+      " 'id', pr.id, " +
+      " 'value', pr.value, " +
+      " 'symbol', pr.symbol, " +
+      " 'is_default', pr.is_default" +
+      " )" +
+      " ) AS prices, " +
+      " json_agg(" +
+      " DISTINCT jsonb_build_object(" +
+      " 'id', g.id, " +
+      " 'start_date', g.start_date, " +
+      " 'end_date', g.end_date" +
+      " )" +
+      " ) AS guarantees " +
+      " FROM products p " +
+      " LEFT JOIN prices pr ON p.id = pr.product_id " +
+      " LEFT JOIN guarantees g ON p.id = g.product_id " +
+      " WHERE p.order_id = $1 " +
+      " GROUP BY p.id " +
+      " LIMIT $2 OFFSET $3;",
       [orderId, limit, offset]
     ),
 };
